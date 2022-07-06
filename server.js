91d854f5a83607c7281edd8b4f80cc51019710ca
xandroezeh <xandro101@gmail.com>
2022-07-06 19:20:14 +0100

    First Commit as Project just kicked off

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+//connect to MonogoDB
+const DB_URL = process.env.MONGODB_URI;
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => { console.log("DB Connected")}).catch((error) => { console.log(error.message)});
+
+//middlewares
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan("dev"));
+
+//routes
+
+app.get("/", (req, res) => {
+    res.send("Hello POS");
+});
+
+//create PORT
+
+const PORT = process.env.PORT || 5000;
+
+//Listen
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
