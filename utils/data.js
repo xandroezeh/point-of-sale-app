781e61ddf872abd4af411dd06bf25a6329243a7d
xandroezeh <xandro101@gmail.com>
2022-07-06 20:28:14 +0100

    work in progress

---
+++ utils/data.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        name: "Kiddies Shirt & Skirt Ensemble",
+        category: "Children's Wears",
+        price: 8000,
+        image: "/images/1.png"
+    },
+    {
+        name: "He-Lives-in-Me Drop Shoulder Baggy T-Shirt",
+        category: "Women's Wear",
+        price: 9000,
+        image: "/images/2.png"
+    },
+    {
+        name: "Pinafore and Shirt School Uniform Set",
+        category: "School Uniform",
+        price: 13000,
+        image: "/images/3.png"
+    },
+    {
+        name: "Blazer, Shirt and Trouser School Uniform Set",
+        category: "School Uniform",
+        price: 23000,
+        image: "/images/4.jpg"
+    },
+    {
+        name: "Stylish Shirt Dress with Drawstring",
+        category: "Women's Wear",
+        price: 12000,
+        image: "/images/5.png"
+    },
+    {
+        name: "Floral 2 Piece Set",
+        category: "Women's Wear",
+        price: 13000,
+        image: "/images/6.png"
+    },
+]
